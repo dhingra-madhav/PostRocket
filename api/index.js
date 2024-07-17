1c71bb5847abac65685567c2b23a403be0d61ed9
dhingra-madhav <21BCS10568@cuchd.in>
2024-07-17 16:56:42 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
