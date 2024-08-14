6384151329bf5f2e158388b0852da16ead6aa18f
dhingra-madhav <21BCS10568@cuchd.in>
2024-08-14 19:29:44 +0530

    resolved api error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -30,20 +30,20 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   try {
     const validUser = await User.findOne({ email });
 
     if (!validUser) {
-      next(errorHandler(400, "User not found"));
+      return next(errorHandler(400, "User not found"));
     }
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
 
     if (!validPassword) {
-      next(errorHandler(400, "Invalid password"));
+      return next(errorHandler(400, "Invalid password"));
     }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
