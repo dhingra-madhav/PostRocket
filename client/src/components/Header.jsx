f46d4dbe2581a2d9752fc14e5de6f23e8ecfb1b1
dhingra-madhav <21BCS10568@cuchd.in>
2024-09-15 23:23:55 +0530

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
   const path = useLocation().pathname;
+
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <Navbar className="border-b-2">
+      
+      {/* Logo */}
       <Link
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
@@ -15,6 +21,8 @@ const Header = () => {
           Post Rocket
         </span>
       </Link>
+
+      {/* Search box */}
       <form>
         <TextInput
           type="text"
@@ -23,26 +31,69 @@ const Header = () => {
           className="hidden lg:inline"
         />
       </form>
+
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
+
+
       <div className="flex gap-2 md:order-2">
+
+        {/* Dark mode button */}
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
-        </Link>
+        
+        {/* User Profile */}
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+
+            <Dropdown.Divider />
+
+            <Dropdown.Item>Sign out</Dropdown.Item>
+
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
         <Navbar.Toggle />
+
       </div>
+        
+      {/* Collapsable nav links */}
       <Navbar.Collapse>
+
+        {/* Link to: Home */}
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
+        
+        {/* Link to: About */}
         <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
+
       </Navbar.Collapse>
+
     </Navbar>
   );
 };
